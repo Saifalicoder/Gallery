1a410950273e1b7c9055300d09af347e20270743
unknown <saifmulla164@gmail.com>
2021-11-09 01:18:20 +0530

    First commit

---
+++ js/index.js
@@ -0,0 +1,8 @@
+burger = document.querySelector('.nav-toggle')
+navbar=document.querySelector('.nav-bar')
+navList=document.querySelector('.nav-list')
+
+burger.addEventListener('click',()=>{
+    navbar.classList.toggle('resp-nav-bar')
+}
+)
